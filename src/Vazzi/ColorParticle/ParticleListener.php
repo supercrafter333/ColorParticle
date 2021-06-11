cb223f1b3126f08cdc869d98b14257c467ed2201
Vazzi <45903049+VazziDE@users.noreply.github.com>
2021-06-11 21:27:56 +0200

    Removed Spawning by MoveEvent

--- src/Vazzi/ColorParticle/ParticleListener.php
+++ src/Vazzi/ColorParticle/ParticleListener.php
@@ -5,32 +5,10 @@ namespace Vazzi\ColorParticle;
 
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 
 class ParticleListener implements Listener
 {
-
-	public function PlayerMove(PlayerMoveEvent $event)
-	{
-		$player = $event->getPlayer();
-		if(isset(ColorParticle::getInstance()->playerparticle[$player->getName()])) {
-			if (in_array(ColorParticle::getInstance()->playerparticle[$player->getName()], ColorParticle::Colors)) {
-				if (($color = ColorParticle::getInstance()->translateColor(ColorParticle::getInstance()->playerparticle[$player->getName()], $player)) !== null) {
-					if($color !== "Rainbow") {
-						$player->getLevel()->addParticle($color);
-					}else{
-						foreach (ColorParticle::Colors as $onecolor){
-							if($onecolor !== "Rainbow") {
-								$player->getLevel()->addParticle(ColorParticle::getInstance()->translateColor($onecolor, $player));
-							}
-						}
-					}
-				}
-
-			}
-		}
-	}
 
 	public function PlayerLeave(PlayerQuitEvent $event)
 	{
